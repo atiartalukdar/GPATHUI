c8cf71f2ea930dc6230d5cffff185cd4032c181e
Atiar Talukdar <swajan.talukdar@gmail.com>
2019-05-28 09:42:05 +0600

    Initial commit

---
+++ app/src/main/java/com/example/gpathui/Main2Activity.java
@@ -0,0 +1,22 @@
+package com.example.gpathui;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+
+
+    public void next(View view) {
+        //startActivity(new Intent(Main2Activity.this, ));
+    }
+}
